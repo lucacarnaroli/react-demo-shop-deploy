e881818f106260b8a922be1c4b95a801056c0875
luca carnaroli <57666446+lucacarnaroli@users.noreply.github.com>
2024-04-11 16:08:33 +0200

    inserito url pocketbase deploy nel file env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_POCKET_BASE_URL: string
+    readonly VITE_POCKET_DEVELOPMENT_URL: string
+    readonly VITE_POCKET_PRODUCTION_URL: string
 }
